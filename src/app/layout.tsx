8242d2e2174666545a3c9de2a49aa2720435526b
cho <hrea12250815@gmail.com>
2024-03-08 06:26:45 +0900

    もう一度pwa構築

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#b8e986" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
