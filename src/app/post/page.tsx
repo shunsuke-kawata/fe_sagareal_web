490631fc54456a8e05a11c507eed2176a1c6a16a
smaru1111 <marusou20@gmail.com>
2024-03-08 03:22:32 +0900

    mod routing

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Post() {
   return (
     <div>
-      <h1>閲覧画面</h1>
+      <h1>投稿画面</h1>
     </div>
   );
 }
