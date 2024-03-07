754f933275252097adfa36580ccbcfacdf5a3684
smaru1111 <marusou20@gmail.com>
2024-03-08 06:36:39 +0900

    modify db同期実装

--- src/hooks/AuthProvider.tsx
+++ src/hooks/AuthProvider.tsx
@@ -5,17 +5,24 @@ import { AuthService } from "./AuthService";
 import { auth } from "@/libs/firebaseConfig";
 import Login from "@/component/page";
 import SkeletonDisplay from "@/component/SkeletonDisplay";
+import { getAuth } from "firebase/auth";
+import { UpdateUserName } from "./UpdateUserName";
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const me = useAuthStore((state) => state.me);
   const { setAuthUser } = AuthService();
+  const { syncUserName } = UpdateUserName();
+  const auth = getAuth();
+  const user = auth.currentUser;
 
   useEffect(() => {
     // ログイン状態を監視し、変化があったら発動
     const unsubscribe = auth.onAuthStateChanged(async (firebaseUser) => {
       console.log("ログイン状態の変化を検知");
       if (firebaseUser) {
-        setAuthUser(firebaseUser.uid);
+        const { uid, displayName, email } = firebaseUser;
+        syncUserName(uid, displayName ?? "unknown", email ?? "unknown");
+        setAuthUser(uid);
       } else {
         // ログインしていない場合、ユーザー情報を空にする
         setAuthUser(null);
--- src/types/domain/user.ts
+++ src/types/domain/user.ts
@@ -2,8 +2,6 @@ import { User } from "@/types/user";
 
 export type UserEntity = User;
 
-export type CreateUserInput = Omit<User, "createAt" | "updatedAt" | "uid"> & {
-  password: string;
-};
+export type CreateUserInput = Omit<User, "createAt" | "updatedAt">;
 
 export type UpdateUserInput = Omit<Partial<User>, "email" | "createAt">;
