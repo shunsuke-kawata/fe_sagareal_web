419f55d33dc5d2a3f67da282fa4aae55b2060f04
金谷信吾 <kanayashingo@kanayashingonoMacBook-Air.local>
2024-03-08 13:11:25 +0900

    APIを叩く処理を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,70 @@
-"use client";
 import './page.css'; // page.cssをインポート
-import React, { useState } from 'react';
 
-// imgSrcの型定義
-type UnitPostProps = {
-  imgSrc: string;
+let res : any = [
+  {
+    id: 1,
+    front_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    back_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    user_uid: "ef304778ada8a6",
+    delete_flag: false,
+    created_at: {},
+    updated_at: {}
+  },
+  {
+    id: 2,
+    front_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    back_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    user_uid: "ef304778ada8a6",
+    delete_flag: false,
+    created_at: {},
+    updated_at: {}
+  },  {
+    id: 3,
+    front_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    back_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    user_uid: "ef304778ada8a6",
+    delete_flag: false,
+    created_at: {},
+    updated_at: {}
+  },  {
+    id: 4,
+    front_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    back_cam_url: "https://picsum.photos/seed/picsum/400/300",
+    user_uid: "ef304778ada8a6",
+    delete_flag: false,
+    created_at: {},
+    updated_at: {}
+  },
+];
+
+type Response = {
+  front_cam_url: string;
+  back_cam_url: string;
+  user_uid: string;
 };
 
-export const UnitPost = ({ imgSrc }: UnitPostProps) => {
+// const searchDog = async (): Promise<Response> => {
+//   const response = await fetch("https://dog.ceo/api/breeds/image/random");
+//   const res: Response = await response.json();
+//   return res;
+// }
+
+
+export const UnitPost = ({ front_cam_url, back_cam_url, user_uid }: Response) => {
   return (
     <div className="unit-post-container">
-      <div>user_name</div>
-      <img src={imgSrc} alt="dummy" />
+      <p>ユーザID:{user_uid}</p>
+      <img src={front_cam_url} style={{maxWidth: "100%", maxHeight: "100%", objectFit: "contain"}} />
+      <img src={back_cam_url}  style={{maxWidth: "100%", maxHeight: "100%", objectFit: "contain"}} />
     </div>
   );
 };
 
 export const PostsList = () => {
-  // 事前に定義したjpegファイルのパスの配列
-  const images = [
-    "/image01.jpg",
-    "/image02.jpg",
-    "/image04.jpg",
-    "/image05.jpg",
-    "/image06.jpg",
-    // 他のjpegファイルのパス...
-  ];
-
   return (
     <div className=" flex flex-col">
-      {images.map((image, index) => (
-        <UnitPost key={index} imgSrc={image} />
+      {res.map((item:any) => (
+        <UnitPost key={item.id} front_cam_url={item.front_cam_url} back_cam_url={item.back_cam_url} user_uid={item.user_uid} />
       ))}
     </div>
   );
@@ -40,7 +74,9 @@ export default function Home() {
   return (
     <div className="page-content">
       <h1>SagaReal.</h1>
-      <div className="post-button"><button>Post</button></div>
+      <div className="post-button">
+      <a href="./post/page.tsx">POST</a>
+      </div>
       <PostsList />
     </div>
   );
